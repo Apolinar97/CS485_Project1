f0ae3bd4746b9511a486e46651bf60a8fdc61b3d
apolinar camacho <camacho.apolinar97@gmail.com@gmail.com>
2019-02-05 00:26:07 -0800

    First Commit

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class playerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    private int count;
+    public Text countText;
+    public Text winText;
+    public Text quitMessage;
+    public AudioSource collSound;
+
+   void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        quitMessage.text = "";
+        collSound = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown("escape"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 21)
+        {
+            winText.text = "You Win!";
+            quitMessage.text = "ESC to menu";
+
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            collSound.Play();
+            count++;
+            SetCountText();
+        }
+    }
+
+   
+}
+
